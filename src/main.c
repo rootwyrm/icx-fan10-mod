dcb501b6475f35063ff3d71e1b3c81b9654f93c9
Phil Jaenke <prj@rootwyrm.com>
2024-06-30 08:47:38 -0400

    In progress commit, still non-building on windows_amd64

--- src/main.c
+++ src/main.c
@@ -18,6 +18,7 @@
 #include <SD.h>
 #include <SPI.h>
 #include <Adafruit_NeoPixel_ZeroDMA.h>  // WARNING: Must use ZeroDMA!
+#include <TeensyThreads.h>
 
 #define MEASURE_TYPE FREQMEASUREMULTI_RAISING
 
@@ -33,9 +34,7 @@ const float pwm_freq = 25000.0f;
 
 // NeoPixel setup
 #define NEOPIXEL_COUNT 8
-neopixel8 neopixel(NEOPIXEL_COUNT, NEOPIXEL_DATA, NEO_RGB + NEO_KHZ800);
-
-// RESERVED: WiFi
+Adafruit_NeoPixel_ZeroDMA neopixel8(NEOPIXEL_COUNT, NEOPIXEL_DATA, NEO_RGB + NEO_KHZ800);
 
 // RESERVED: SDCard
 
@@ -74,36 +73,41 @@ void setup()
   // RESERVED: SDCard
 
   // Setup neopixel
-  neopixel.begin();
-  pixels.clear();
+  neopixel8.begin();
+  neopixel8.setBrightness(128);
+  neopixel8.clear();
   // Do a color cycle test to prove life
   for(int i=0; i<4; i++) {
     // i=0, red; i=1, green; i=2, blue; i=3, clear
     for(int j=0; j<NEOPIXEL_COUNT; j++) {
-      pixels.setPixelColor(j, pixels.Color(i==0?255:0, i==1?255:0, i==2?255:0, i==3?0:0));
-      pixels.show();
+      neopixel8.setPixelColor(j, neopixel8.Color(i==0?255:0, i==1?255:0, i==2?255:0, i==3?0:0));
+      neopixel8.show();
       delay(250);
     }
   }
   // Clear pixels again
-  pixels.clear();
+  neopixel8.clear();
 
+  // Setup FAULT_LED
   pinMode(FAULT_LED, OUTPUT);
   // Test our fault LED
-  log_console("Testing FAULT LED.");
-  digitalWrite(FAULT_LED, HIGH);
-  delay(500);
-  digitalWrite(FAULT_LED, LOW);
-
+  log_console("Testing FAULT LED - you should see it blinking.");
+  for (int i = 0; i < 20; i++)
+  {
+    digitalWrite(FAULT_LED, HIGH);
+    delay(50);
+    digitalWrite(FAULT_LED, LOW);
+    delay(50);
+  }
 
   // Set up outputs and initialize high
   pinMode(FAN0_PWM, OUTPUT);
   pinMode(FAN1_PWM, OUTPUT);
-  // XXX: resolution 12 doesn't work as expected
-  analogWriteResolution(8);
   analogWriteFrequency(FAN0_PWM, pwm_freq);
   analogWriteFrequency(FAN1_PWM, pwm_freq);
-  // WARNING: always start fans at 100% 
+  // XXX: resolution 12 doesn't work as expected
+  analogWriteResolution(8);
+  // Always start fans at 100% to ensure we don't cook off.
   pwm_percent(FAN0_PWM, 100.0);
   pwm_percent(FAN1_PWM, 100.0);
 
@@ -114,13 +118,13 @@ void setup()
   icx_mod1_pwm.begin(ICX_MOD1_PWM);
 
   // Set up tachometer outputs for ICX
-  pinMode(ICX_MOD0_TACH0, OUTPUT_OPEN_DRAIN);
-  pinMode(ICX_MOD0_TACH1, OUTPUT_OPEN_DRAIN);
-  pinMode(ICX_MOD1_TACH0, OUTPUT_OPEN_DRAIN);
-  pinMode(ICX_MOD1_TACH1, OUTPUT_OPEN_DRAIN);
+  pinMode(ICX_MOD0_TACH0, OUTPUT_OPENDRAIN);
+  pinMode(ICX_MOD0_TACH1, OUTPUT_OPENDRAIN);
+  pinMode(ICX_MOD1_TACH0, OUTPUT_OPENDRAIN);
+  pinMode(ICX_MOD1_TACH1, OUTPUT_OPENDRAIN);
 
   // Modification fan inputs; use care here.
-  // Per Intel spec, normal is a 10k pullup from 12V supply; we're using a 3.3V with a split to limit to 1.65V
+  // Per Intel spec, normal is a 10k pullup from 12V supply; we're using a 1k from 5V
   pinMode(FAN0_TACH, INPUT);
   pinMode(FAN1_TACH, INPUT);
 
@@ -129,6 +133,12 @@ void setup()
   log_console("FAN0 Tachometer interrupt attached.");
   attachInterrupt(digitalPinToInterrupt(FAN1_TACH), fan1_tach_isr, FALLING);
   log_console("FAN1 Tachometer interrupt attached.");
+
+  // Spawn threads
+  log_console("Spawning operational threads...");
+  threads.addThread(icx_module_control, "mod0");
+  threads.addThread(icx_module_control, "mod1");
+
   log_console("System boot complete!");
 }
 
@@ -148,27 +158,34 @@ void loop()
   fan0_rpm = fan0_tach * 60 / 2;
   fan1_rpm = fan1_tach * 60 / 2;
 
+  // Create a thread for module 0 operations
+
+
   // TODO: this should be moved to a thread that has an internal delay
   if (mod10_pwm_update_timer > 1000)
   {
     // Update the PWM outputs
-    icx_module_pwm_update(char "mod0", int ICX_MOD0_PWM);
-    icx_module_pwm_update(char "mod1", int ICX_MOD1_PWM);
+    icx_module_pwm_update("mod0", ICX_MOD0_PWM);
+    // TODO: Update the Neopixel
+    icx_module_pwm_update("mod1", ICX_MOD1_PWM);
+    // Update the Neopixel
     mod10_pwm_update_timer = 0;
   }
 
   // TODO: this could possibly be threaded too?
   if (fan0_tach_timer > 1000)
   {
-    /* Update tachometer output */
+    /* TODO: Update tachometer output */
     fan0_tach = 0;
     fan0_tach_timer = 0;
+    // Update the Neopixel
   }
   if (fan1_tach_timer > 1000)
   {
-    /* Update tachometer output */
+    /* TODO: Update tachometer output */
     fan1_tach = 0;
     fan1_tach_timer = 0;
+    // Update the Neopixel
   }
 
   if (debug_print_timer > 5000)
@@ -238,4 +255,65 @@ void fan0_tach_isr()
 void fan1_tach_isr()
 {
   fan1_tach++;
+}
+
+// THREAD: icx_module_control
+// Performs the ICX module control operations, including PWM and tachometer feeding
+void icx_module_control(char mod)
+{
+  int PWM, tach0, tach1;
+  float pwm_command;
+  pwm_func_t* pwm_func = NULL;
+
+  if (mod == "mod0") 
+  {
+    pwm_func = &icx_mod0_pwm;
+    PWM = ICX_MOD0_PWM;
+    tach0 = ICX_MOD0_TACH0;
+    tach1 = ICX_MOD0_TACH1;
+  }
+  else if (mod == "mod1") 
+  {
+    pwm_func = &icx_mod1_pwm;
+    PWM = ICX_MOD1_PWM;
+    tach0 = ICX_MOD1_TACH0;
+    tach1 = ICX_MOD1_TACH1;
+  } 
+
+  // Catch error
+  if (pwm_func == NULL) {
+    log_console("icx_module_control: could not find pwm_func!");
+    ASSERT();
+    return;
+  }
+
+  pwm_command = pwm_func->read();
+  if (pwm_command != pwm_func->last_read()) {
+    // Update the PWM output
+    analogWrite(PWM, pwm_command);
+    log_console("PWM updated to " + pwm_command);
+  }
+
+}
+
+// THREAD: module0_status_led
+// Monitors the MOD0_OK and MOD0_FAULT signals, and updates the status LED
+void module0_status_led()
+{
+  int read_ok, read_fault;
+  // Do a digital read of MOD0_OK and MOD0_FAULT
+  read_ok = digitalRead(ICX_MOD0_OK);
+  read_fault = digitalRead(ICX_MOD0_FAULT);
+
+  // Update the status LED and neopixel LED 1
+  if (read_ok == HIGH && read_fault == LOW) {
+    digitalWrite(FAULT_LED, LOW);
+    neopixel8.setPixelColor(1, neopixel8.Color(0, 255, 0, 0));
+  } else if (read_ok == LOW && read_fault == HIGH) {
+    digitalWrite(FAULT_LED, HIGH);
+    neopixel8.setPixelColor(1, neopixel8.Color(255, 0, 0, 0));
+  } else {
+    digitalWrite(FAULT_LED, HIGH);
+    neopixel8.setPixelColor(1, neopixel8.Color(255, 255, 0, 0));
+  }
 }
